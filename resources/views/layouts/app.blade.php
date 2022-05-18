dff6572e05c478c4c6595dc70c1b659b0f1d0a74
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-18 14:52:23 +0200

    add header + css + new views comics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,17 @@
-@extends('layouts.app') 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <link rel="stylesheet" href="css/app.css">
+    <title>@yield('title')</title>
 </head>
 <body>
-    @include('partials/header')
-    <p>home</p>
+    @include ('partials/header')
+    <main>
+        @yield('content')
+       
+    </main>
 </body>
 </html>
